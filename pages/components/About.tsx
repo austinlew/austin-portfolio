044d4fdb69e30f8dffc6aa156676da5db9fa4723
Austin Lew <bubbalew81@email.com>
2023-08-28 22:08:32 -0700

    updated

--- pages/components/About.tsx
+++ pages/components/About.tsx
@@ -25,7 +25,7 @@ const About = () => {
             <span>{about.secondary}</span>
           </div>
           <div className="mt-6 mx-auto md:mx-0 border-2 border-white py-1 px-2 w-36 text-center font-bold">
-            <a href={'#'} className="about__resume text-white text-l">View Resume</a>
+            <a href={'https://docs.google.com/document/d/1g3xwDulwinVZjfXq94jKFphlEkF8cU6OmgtDIfX8jY8/edit'} className="about__resume text-white text-l">View Resume</a>
           </div>
         </div>
       </div>
